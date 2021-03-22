960a0fda12a64370186406185a62f5534c97a6d9
mdk-klm <antoine.04@laposte.net>
2021-03-22 12:27:29 +0100

    add arabic to roman

--- convert.js
+++ convert.js
@@ -1,5 +1,3 @@
-var express = require("express");
-
 //test ci
 
 const values = new Map([
@@ -28,4 +26,48 @@ function convertNum(string) {
   }
   return result;
 }
+
+var arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+  roman = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ],
+  arabicLength = arabic.length;
+
+// nombre arabe to nombre romain
+
+function toRoman(nombre) {
+  var x = parseInt(nombre, 10) || 1,
+    string = "";
+
+  if (x < 1) {
+    x = 1;
+  }
+
+  for (var i = 0; i < arabicLength; ++i) {
+    while (x >= arabic[i]) {
+      x -= arabic[i];
+      string += roman[i];
+    }
+
+    if (x == 0) {
+      break;
+    }
+  }
+
+  return string;
+}
+
 module.exports = convertNum;
+module.exports = toRoman;
--- convert.test.js
+++ convert.test.js
@@ -1,13 +1,18 @@
-const convert= require('./convert');
+const convertNum = require("./convert");
+const convert = require("./convert");
+const toRoman = require("./convert");
 
-test('I = 1',() => {
-  expect(convert("I")).toBe(1);
-  expect(convert("II")).toBe(2);
-  expect(convert("III")).toBe(3);
-  expect(convert("IV")).toBe(4);
-  expect(convert("X")).toBe(10);
-  expect(convert("CXV")).toBe(115);
-  expect(convert("CCCXXVII")).toBe(327);
-  expect(convert("MXI")).toBe(1011);
-  expect(convert("MMXXI")).toBe(2021);
+test("I = 1", () => {
+  expect(convertNum("II")).toBe(2);
+  expect(convertNum("III")).toBe(3);
+  expect(convertNum("IV")).toBe(4);
+  expect(convertNum("X")).toBe(10);
+  expect(convertNum("CXV")).toBe(115);
+  expect(convertNum("CCCXXVII")).toBe(327);
+  expect(convertNum("MXI")).toBe(1011);
+  expect(convertNum("MMXXI")).toBe(2021);
+});
+
+test('1 = "I', () => {
+  expect(toRoman(1)).toBe("I");
 });
